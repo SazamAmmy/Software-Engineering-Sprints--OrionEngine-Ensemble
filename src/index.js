dd36f6bd4e556b7f1146d72ca5b9a591e93b9df2
Raja108N <159127703+Raja108N@users.noreply.github.com>
2024-04-23 13:34:22 +0100

    Add files via upload
    Co-authored-by: Sudip Thapa Magar <124052469+SazamAmmy@users.noreply.github.com>
    Co-authored-by: Joys1 <158183077+shjoy11@users.noreply.github.com>

--- src/index.js
+++ src/index.js
@@ -12,15 +12,25 @@ app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "static-css-files")));
 
 const homeRoute = require('./routes/home.route.js');
+const sub_dashboardRoute = require('./routes/sub_dashboard.route.js');
+const contactUsRoute = require('./routes/contact_us.route.js');
 const aboutRoute = require('./routes/about_us.route.js');
 const citiesRoute = require('./routes/cities.route.js');
+const countriesRoute = require('./routes/countries.route.js');
+const capitalCitiesRoute = require('./routes/capitalCities.route.js');
+const languagesRoute = require('./routes/languages.route.js');
 const signupRoute = require('./routes/signup.route.js');
 const loginRoute = require('./routes/login.route.js');
 
 // Using routes
 app.use('/', homeRoute);
+app.use('/sub_dashboard', sub_dashboardRoute);
+app.use('/contact_us', contactUsRoute);
 app.use('/about_us', aboutRoute);
 app.use('/cities', citiesRoute);
+app.use('/countries', countriesRoute);
+app.use('/capitalCities', capitalCitiesRoute);
+app.use('/languages', languagesRoute);
 app.use('/signup', signupRoute);
 app.use('/login', loginRoute);
 
